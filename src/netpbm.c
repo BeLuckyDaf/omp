1a2ae774216d5c1a155d95490514c47a13478b77
Vladislav Smirnov <beluckydaf@gmail.com>
2019-04-16 11:55:57 +0300

    initial commit

---
+++ src/netpbm.c
@@ -0,0 +1,80 @@
+#include "netpbm.h"
+
+
+/**
+ * Allocates memory for the given dimensions of an RGB image
+ * and returns a pointer to the rgb_image structure
+ */
+struct rgb_image* create_rgb_image(uint width, uint height) {
+    // allocate for the structure
+    struct rgb_image* image = (struct rgb_image*)malloc(sizeof(struct rgb_image));
+
+    // set the dimensions
+    image->width = width;
+    image->height = height;
+
+    // allocate a pointer to hold pointers to rows
+    // and pointers for the rows themselves
+    image->matrix = (struct rgb_color**)calloc(height, sizeof(struct rgb_color*));
+    for (int i = 0; i < height; i++) {
+        image->matrix[i] = (struct rgb_color*)calloc(width, sizeof(struct rgb_color));
+    }
+
+    return image;
+}
+
+/**
+ * Completely frees the allocated memory for the RGB image structure
+ */
+void free_rgb_image(struct rgb_image *image) {
+    for (int i = 0; i < image->height; i++) {
+        free(image->matrix[i]); // free each row
+    }
+
+    // then free the matrix and the image struct itself
+    free(image->matrix);
+    free(image);
+}
+
+/**
+ * Allocates memory for the given dimensions of a grayscale image
+ * and returns a pointer to the grayscale_image structure
+ */
+struct grayscale_image* create_grayscale_image(uint width, uint height) {
+    // allocate for the structure
+    struct grayscale_image* image = (struct grayscale_image*)malloc(sizeof(struct grayscale_image));
+
+    // set the dimensions
+    image->width = width;
+    image->height = height;
+
+    // allocate a pointer to hold pointers to rows
+    // and pointers for the rows themselves
+    image->matrix = (uint**)calloc(height, sizeof(uint*));
+    for (int i = 0; i < height; i++) {
+        image->matrix[i] = (uint*)calloc(width, sizeof(uint));
+    }
+
+    return image;
+}
+
+/**
+ * Completely frees the allocated memory for the grayscale image structure
+ */
+void free_grayscale_image(struct grayscale_image *image) {
+    for (int i = 0; i < image->height; i++) {
+        free(image->matrix[i]); // free each row
+    }
+
+    // then free the matrix and the image struct itself
+    free(image->matrix);
+    free(image);
+}
+
+/**
+ * Allocates memory for the new grayscale image and then find average color
+ * for each pixel and thus converts colored image to the grayscale
+ */
+struct grayscale_image* rgb_to_grayscale_image(struct rgb_image* image) {
+    return NULL;
+}
---
+++ src/netpbm.h
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+typedef unsigned int uint;
+
+/**
+ * Contains RGB data for a single pixel
+ */
+struct rgb_color {
+    uint r, g, b;
+};
+
+/**
+ * Contains data about an RGB image in a 2D matrix, where an element
+ * of the matrix is an array, which consists of 3 elements: R, G and B
+ * color channel values
+ */
+struct rgb_image {
+    uint width;
+    uint height;    
+    struct rgb_color **matrix;
+};
+
+/**
+ * Contains data about an RGB image in a 2D matrix, where an element
+ * of the matrix is a grayscale value
+ */
+struct grayscale_image {
+    uint width;
+    uint height;    
+    uint **matrix; // same as rgb, but no need for third level array, just a number
+};
+
+// Functions working with memory
+struct rgb_image* create_rgb_image(uint width, uint height);
+struct grayscale_image* create_grayscale_image(uint width, uint height);
+struct grayscale_image* rgb_to_grayscale_image(struct rgb_image* image);
+void free_rgb_image(struct rgb_image *image);
+void free_grayscale_image(struct grayscale_image *image);
